35b8b04d4b727ea7f247c1b3e558ebb52a68a979
kaustoobhwaghmare-debug <kaustoobhwaghmare@gmail.com>
2026-08-27 14:00:07 +0530

    squre

---
+++ Squre.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	  int i,n,sandesh;
+	  printf("enter limit:");
+	  scanf("%d",&n);
+	  
+	  printf("squre of  number=");
+	  for(i=1; i<=n; i++)
+	  {
+	    sandesh=i*i;
+	  	printf("\n%d",sandesh);
+	  }
+}
